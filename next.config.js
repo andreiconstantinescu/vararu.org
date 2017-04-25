70d14d244866b37d0c7fa7f66a479969bb419ccf
Theodor Vararu <theo@vararu.org>
2017-04-25 01:28:44 +0100

    Only add webpack visualizer plugin in dev

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const Visualizer = require('webpack-visualizer-plugin')
-
 module.exports = {
-  webpack: (config) => {
-    config.plugins.push(new Visualizer())
+  webpack: (config, {dev}) => {
+    if (dev) {
+      const Visualizer = require('webpack-visualizer-plugin')
+      config.plugins.push(new Visualizer())
+    }
     return config
   }
 }
