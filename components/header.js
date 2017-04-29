105697588ddc5e9977351f910a9dc6d40119ceb9
Theodor Vararu <theo@vararu.org>
2017-04-29 21:02:03 +0100

    Move header into separate component

---
+++ components/header.js
@@ -0,0 +1,74 @@
+// @flow
+import {Component} from 'react'
+import Logo from './logo'
+
+const hasRAF = (ctx): boolean => !!ctx.requestAnimationFrame
+
+export default class Header extends Component {
+  state: {
+    renderLogo: boolean
+  }
+
+  constructor (props: Object) {
+    super(props)
+
+    this.state = {
+      renderLogo: false
+    }
+  }
+
+  componentDidMount () {
+    if (hasRAF(window)) {
+      this.setState({renderLogo: true})
+    }
+  }
+
+  render () {
+    const {renderLogo} = this.state
+
+    return (
+      <header>
+        <style jsx>{`
+          header, .logo {
+            height: 4rem;
+            font-weight: bold;
+          }
+
+          header {
+            align-items: center;
+            display: flex;
+            justify-content: space-between;
+            margin: 2rem 0;
+          }
+
+          p, a {
+            color: currentColor;
+            font-size: 1.25rem;
+          }
+
+          .logo {
+            perspective-origin: 50% 50%;
+            perspective: 512px;
+            width: 8rem;
+          }
+
+          @media (min-width: 30rem) {
+            header, .logo {
+              height: 5rem;
+            }
+
+            p, a {
+              font-size: 1.5rem;
+            }
+
+            .logo {
+              width: 10rem;
+            }
+          }
+        `}</style>
+        <p>theodor<br /><a href="/">vararu.org</a></p>
+        {renderLogo && <div className="logo"><Logo /></div>}
+      </header>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,9 @@
 // @flow
 import {Component} from 'react'
-import Logo from '../components/logo'
-
-const hasRAF = (ctx): boolean => !!ctx.requestAnimationFrame
+import Header from '../components/header'
 
 export default class Index extends Component {
-  state: {
-    renderLogo: boolean
-  }
-
-  constructor (props: Object) {
-    super(props)
-
-    this.state = {
-      renderLogo: false
-    }
-  }
-
-  componentDidMount () {
-    if (hasRAF(window)) {
-      this.setState({renderLogo: true})
-    }
-  }
-
   render () {
-    const {renderLogo} = this.state
-
     return (
       <div>
         <style jsx>{`
@@ -33,7 +11,7 @@ export default class Index extends Component {
             margin: 1rem;
           }
 
-          h1, p, ul {
+          p, ul {
             font-size: 1.25rem;
           }
 
@@ -50,42 +28,19 @@ export default class Index extends Component {
             margin: 1rem 0;
           }
 
-          .title {
-            align-items: center;
-            display: flex;
-            justify-content: space-between;
-            margin: 2rem 0;
-          }
-
-          .logo-wrapper {
-            height: 4rem;
-            margin: 0.25rem 0 0 1rem;
-            perspective-origin: 50% 50%;
-            perspective: 512px;
-            width: 8rem;
-          }
-
           @media (min-width: 30rem) {
             main {
               margin: 3rem auto;
               max-width: 30rem;
             }
 
-            h1, p, ul {
+            p, ul {
               font-size: 1.5rem;
-            }
-
-            .logo-wrapper {
-              height: 5rem;
-              width: 10rem;
             }
           }
         `}</style>
         <main>
-          <div className="title">
-            <h1>theodor<br />vararu.</h1>
-            {renderLogo && <div className="logo-wrapper"><Logo /></div>}
-          </div>
+          <Header />
           <p>
             Theo is a JavaScript developer interested in accessibility, Node.js, and React.js.
           </p>
